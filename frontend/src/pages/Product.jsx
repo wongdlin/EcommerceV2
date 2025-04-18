5a6fee1b38e2f6ae9dca0fa1227c0020d0056686
wongdlin <wongdlin2000@gmail.com>
2025-04-18 14:59:52 +0800

    mc testing

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,11 +1,15 @@
-import Product from "../components/product"
+import Product from "../components/product";
+import { useEffect } from "react";
 
-function Products(){
-
-    return (
-        <div>
-            <Product/>
-        </div>
-    )
-
-}export default Products
+function Products() {
+    fetch('http://localhost:5000/api/products')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+  return (
+    <div>
+      <Product />
+    </div>
+  );
+}
+export default Products;
