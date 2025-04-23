b929a08554f1136d060155610726c7631fb9dd64
wongdlin <wongdlin2000@gmail.com>
2025-04-23 14:37:30 +0800

    moved components/product code to pages/product, deleted components/product

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,14 +1,134 @@
-import Product from "../components/Product";
-import Spinner from "../components/ui/spinner";
+// import Product from "../components/Product";
+import { useState } from "react";
 import useFetch from "../hooks/useFetch";
+import Spinner from "../components/ui/spinner";
+import Breadcrumb from "../components/ui/breadcrumb";
+import ProductImgGallery from "../components/productImgGallery";
+import Reviews from "../components/ui/reviews";
+import ColorPicker from "../components/colorPicker";
+import SizePicker from "../components/sizePicker";
+import Error from "../components/error";
+import { useParams } from "react-router-dom";
+
+const product = {
+  name: 'Basic Tee 6-Pack',
+  price: '$192',
+  href: '#',
+  breadcrumbs: [
+    { id: 1, name: 'Men', href: '#' },
+    { id: 2, name: 'Clothing', href: '#' },
+  ],
+  colors: [
+    { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
+    { name: 'Gray', class: 'bg-gray-200', selectedClass: 'ring-gray-400' },
+    { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
+  ],
+  sizes: [
+    { name: 'XXS', inStock: false },
+    { name: 'XS', inStock: true },
+    { name: 'S', inStock: true },
+    { name: 'M', inStock: true },
+    { name: 'L', inStock: true },
+    { name: 'XL', inStock: true },
+    { name: '2XL', inStock: true },
+    { name: '3XL', inStock: true },
+  ],
+  description:
+    'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+  highlights: [
+    'Hand cut and sewn locally',
+    'Dyed with our proprietary colors',
+    'Pre-washed & pre-shrunk',
+    'Ultra-soft 100% cotton',
+  ],
+  details:
+    'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+}
+const reviews = { href: '#', average: 4, totalCount: 117 }
 
 function Products() {
 
-  // const {data: product, loading, error} = useFetch("/api/products/1")
+  const [selectedColor, setSelectedColor] = useState(product.colors[0])
+  const [selectedSize, setSelectedSize] = useState(product.sizes[2])
+  const {id} = useParams()
+  const {data: test, loading, error} = useFetch(`/api/products/${id}`)
+  console.log("test",test)
 
-  // if (loading) return <Spinner/>
-  // if(error) return <p className="text-red-500">{error}</p>
+  if (loading) return <Spinner/>
+  if(error) return <Error errMsg={error}/>
 
-  return <Product/>
+  return (
+    <div className="bg-white">
+      <Breadcrumb product={test}/>
+      <div className="pt-6">
+        
+        <ProductImgGallery data={test.images}/>
+
+        {/* Product info */}
+        <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto_auto_1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
+          <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+            <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{test.name}</h1>
+          </div>
+
+          {/* Options */}
+          <div className="mt-4 lg:row-span-3 lg:mt-0">
+            <h2 className="sr-only">Product information</h2>
+            <p className="text-3xl tracking-tight text-gray-900">RM{test.price}</p>
+
+            {/* Reviews */}
+            <Reviews reviews={reviews}/>
+
+            <form className="mt-10">
+              {/* Colors */}
+              <ColorPicker selectedColor={selectedColor} setSelectedColor={setSelectedColor} colors={product.colors}/>
+
+              {/* Sizes */}
+              <SizePicker selectedSize={selectedSize} setSelectedSize={setSelectedSize} sizes={product.sizes}/>
+
+              <button
+                type="submit"
+                className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:outline-hidden"
+              >
+                Add to bag
+              </button>
+            </form>
+          </div>
+
+          <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pr-8 lg:pb-16">
+            {/* Description and details */}
+            <div>
+              <h3 className="sr-only">Description</h3>
+
+              <div className="space-y-6">
+                <p className="text-base text-gray-900">{test.description}</p>
+              </div>
+            </div>
+
+            <div className="mt-10">
+              <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
+
+              <div className="mt-4">
+                <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
+                  {product.highlights.map((highlight) => (
+                    <li key={highlight} className="text-gray-400">
+                      <span className="text-gray-600">{highlight}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            <div className="mt-10">
+              <h2 className="text-sm font-medium text-gray-900">Details</h2>
+
+              <div className="mt-4 space-y-6">
+                <p className="text-sm text-gray-600">{test.description}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
-export default Products;
+export default Products
